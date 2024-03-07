e19b7237cf42e4ce63386650e816a79137018280
SaxenaSim <saxenasimran84@gmail.com>
2024-03-07 10:56:54 +0530

    Add .gitignore

--- tests/test_detection_function.py
+++ tests/test_detection_function.py
@@ -3,22 +3,26 @@
 # sys.path.insert(0,os.path.abspath("../data_detection"))
 from data_detection.detection import detect_secrets
 
+
 def test_sensitive_info():
-    example_string="String contains Username"
+    example_string = "String contains Username"
     assert detect_secrets(example_string) == True
 
+
 # Output : Test case passed
+
 
 def test_no_sensitive_info():
-    example_string="String does not contain sensitive data"
+    example_string = "String does not contain sensitive data"
     assert detect_secrets(example_string) == False
 
+
 # Output : Test case passed
+
 
 def test_no_data():
-    example_string=" "
+    example_string = " "
     assert detect_secrets(example_string) == False
 
+
 # Output : Test case passed
-
-
